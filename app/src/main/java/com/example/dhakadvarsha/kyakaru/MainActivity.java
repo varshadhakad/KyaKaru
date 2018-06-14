90467d253051c333d1d5cdfb773226129ca4fa19
Khushboo Sharma <khush30796@outlook.com>
2018-06-14 16:18:05 +0530

    created utils class

--- app/src/main/java/com/example/dhakadvarsha/kyakaru/MainActivity.java
+++ app/src/main/java/com/example/dhakadvarsha/kyakaru/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
